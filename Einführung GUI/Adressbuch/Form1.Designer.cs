c30048267d9a8ee5e10c304816da55665a01de03
Circlebit <paul.seibel@mailbox.org>
2018-10-15 12:27:29 +0200

    mehr

--- Einführung GUI/Adressbuch/Form1.Designer.cs
+++ Einführung GUI/Adressbuch/Form1.Designer.cs
@@ -30,12 +30,13 @@
         {
             this.textBox_FirstName = new System.Windows.Forms.TextBox();
             this.groupBox_NewEntry = new System.Windows.Forms.GroupBox();
-            this.label_FirstName = new System.Windows.Forms.Label();
-            this.label_LastName = new System.Windows.Forms.Label();
-            this.dateTimePicker_Birthday = new System.Windows.Forms.DateTimePicker();
-            this.label_Birthday = new System.Windows.Forms.Label();
-            this.textBox_LastName = new System.Windows.Forms.TextBox();
             this.button_AddEntry = new System.Windows.Forms.Button();
+            this.textBox_LastName = new System.Windows.Forms.TextBox();
+            this.label_Birthday = new System.Windows.Forms.Label();
+            this.dateTimePicker_Birthday = new System.Windows.Forms.DateTimePicker();
+            this.label_LastName = new System.Windows.Forms.Label();
+            this.label_FirstName = new System.Windows.Forms.Label();
+            this.showPersons = new System.Windows.Forms.Button();
             this.groupBox_NewEntry.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -62,30 +63,22 @@
             this.groupBox_NewEntry.TabStop = false;
             this.groupBox_NewEntry.Text = "Neuer Eintrag";
             // 
-            // label_FirstName
+            // button_AddEntry
             // 
-            this.label_FirstName.AutoSize = true;
-            this.label_FirstName.Location = new System.Drawing.Point(6, 26);
-            this.label_FirstName.Name = "label_FirstName";
-            this.label_FirstName.Size = new System.Drawing.Size(49, 13);
-            this.label_FirstName.TabIndex = 1;
-            this.label_FirstName.Text = "Vorname";
+            this.button_AddEntry.Location = new System.Drawing.Point(198, 105);
+            this.button_AddEntry.Name = "button_AddEntry";
+            this.button_AddEntry.Size = new System.Drawing.Size(75, 23);
+            this.button_AddEntry.TabIndex = 6;
+            this.button_AddEntry.Text = "Hinzufügen";
+            this.button_AddEntry.UseVisualStyleBackColor = true;
+            this.button_AddEntry.Click += new System.EventHandler(this.button_AddEntry_Click);
             // 
-            // label_LastName
+            // textBox_LastName
             // 
-            this.label_LastName.AutoSize = true;
-            this.label_LastName.Location = new System.Drawing.Point(6, 52);
-            this.label_LastName.Name = "label_LastName";
-            this.label_LastName.Size = new System.Drawing.Size(59, 13);
-            this.label_LastName.TabIndex = 2;
-            this.label_LastName.Text = "Nachname";
-            // 
-            // dateTimePicker_Birthday
-            // 
-            this.dateTimePicker_Birthday.Location = new System.Drawing.Point(74, 74);
-            this.dateTimePicker_Birthday.Name = "dateTimePicker_Birthday";
-            this.dateTimePicker_Birthday.Size = new System.Drawing.Size(199, 20);
-            this.dateTimePicker_Birthday.TabIndex = 3;
+            this.textBox_LastName.Location = new System.Drawing.Point(74, 49);
+            this.textBox_LastName.Name = "textBox_LastName";
+            this.textBox_LastName.Size = new System.Drawing.Size(199, 20);
+            this.textBox_LastName.TabIndex = 5;
             // 
             // label_Birthday
             // 
@@ -96,27 +89,47 @@
             this.label_Birthday.TabIndex = 4;
             this.label_Birthday.Text = "Geburtstag";
             // 
-            // textBox_LastName
+            // dateTimePicker_Birthday
             // 
-            this.textBox_LastName.Location = new System.Drawing.Point(74, 49);
-            this.textBox_LastName.Name = "textBox_LastName";
-            this.textBox_LastName.Size = new System.Drawing.Size(199, 20);
-            this.textBox_LastName.TabIndex = 5;
+            this.dateTimePicker_Birthday.Location = new System.Drawing.Point(74, 74);
+            this.dateTimePicker_Birthday.Name = "dateTimePicker_Birthday";
+            this.dateTimePicker_Birthday.Size = new System.Drawing.Size(199, 20);
+            this.dateTimePicker_Birthday.TabIndex = 3;
             // 
-            // button_AddEntry
+            // label_LastName
             // 
-            this.button_AddEntry.Location = new System.Drawing.Point(198, 105);
-            this.button_AddEntry.Name = "button_AddEntry";
-            this.button_AddEntry.Size = new System.Drawing.Size(75, 23);
-            this.button_AddEntry.TabIndex = 6;
-            this.button_AddEntry.Text = "Hinzufügen";
-            this.button_AddEntry.UseVisualStyleBackColor = true;
+            this.label_LastName.AutoSize = true;
+            this.label_LastName.Location = new System.Drawing.Point(6, 52);
+            this.label_LastName.Name = "label_LastName";
+            this.label_LastName.Size = new System.Drawing.Size(59, 13);
+            this.label_LastName.TabIndex = 2;
+            this.label_LastName.Text = "Nachname";
+            // 
+            // label_FirstName
+            // 
+            this.label_FirstName.AutoSize = true;
+            this.label_FirstName.Location = new System.Drawing.Point(6, 26);
+            this.label_FirstName.Name = "label_FirstName";
+            this.label_FirstName.Size = new System.Drawing.Size(49, 13);
+            this.label_FirstName.TabIndex = 1;
+            this.label_FirstName.Text = "Vorname";
+            // 
+            // showPersons
+            // 
+            this.showPersons.Location = new System.Drawing.Point(21, 173);
+            this.showPersons.Name = "showPersons";
+            this.showPersons.Size = new System.Drawing.Size(129, 23);
+            this.showPersons.TabIndex = 2;
+            this.showPersons.Text = "Personen anzeigen";
+            this.showPersons.UseVisualStyleBackColor = true;
+            this.showPersons.Click += new System.EventHandler(this.showPersons_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showPersons);
             this.Controls.Add(this.groupBox_NewEntry);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -136,6 +149,7 @@
         private System.Windows.Forms.Label label_Birthday;
         private System.Windows.Forms.DateTimePicker dateTimePicker_Birthday;
         private System.Windows.Forms.Button button_AddEntry;
+        private System.Windows.Forms.Button showPersons;
     }
 }
 
--- Einführung GUI/Adressbuch/Form1.cs
+++ Einführung GUI/Adressbuch/Form1.cs
@@ -12,10 +12,30 @@ namespace Adressbuch
 {
     public partial class Form1 : Form
     {
+        private List<Person> Persons { get; set; }
+
         public Form1()
         {
             InitializeComponent();
+            Persons = new List<Person>();
         }
 
+        private void button_AddEntry_Click(object sender, EventArgs e)
+        {
+            Person newPerson = new Person();
+            newPerson.FirstName = textBox_FirstName.Text;
+            newPerson.LastName = textBox_LastName.Text;
+            newPerson.Birthday = dateTimePicker_Birthday.Value.Date;
+        }
+
+        private void showPersons_Click(object sender, EventArgs e)
+        {
+            string personsTxt = "";
+            foreach(Person p in Persons)
+            {
+                personsTxt += (p.FirstName + " " + p.LastName + " " + p.Birthday.ToString() + "\n");
+            }
+            MessageBox.Show(personsTxt);
+        }
     }
 }
